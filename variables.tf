190a3a637aa2c14fb0e8e1d79c50d7ac0c8f2dd0
yarraradha <43906675+yarraradha@users.noreply.github.com>
2022-09-06 17:25:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,10 @@ variable "project_id" {
   type        = string
   description = "(Required) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
 }
+variable "alert_id" {
+  type        = string
+  description = "(Required) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+}
 
 variable "combiner" {
   type        = string
