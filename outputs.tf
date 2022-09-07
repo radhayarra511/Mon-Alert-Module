42d9c625081c54e1191d8755c8b3136b24a714ab
yarraradha <43906675+yarraradha@users.noreply.github.com>
2022-09-07 12:39:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,3 @@
-output "alert_policy_id" {
-  value=google_monitoring_alert_policy.alert_policy.id
-  description = "ID of the created Alert policy"
+output "alert_id" {
+value=google_monitoring_alert_policy.alert_policy.id
 }
-
